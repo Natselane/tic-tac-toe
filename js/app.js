f9a7e5ae322030d77cb29def56d58f9a80386530
Natselane <fabien1308@gmail.com>
2020-12-29 22:23:18 +0100

    add computer play functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const gameBoard = (() => {
+const GameBoard = (() => {
     let gameArray = {
         0: ["free", "o"],
         1: ["free", "x"],
@@ -16,7 +16,7 @@ const gameBoard = (() => {
     };
 })();
 
-const displayController = (() => {
+const DisplayController = (() => {
     // Make a player
     const playerFactory = (name, marker) => {
         return { name, marker };
@@ -24,10 +24,47 @@ const displayController = (() => {
 
     let player; 
     let computer;
+    let gameCount = 0;
 
+    const placeMarker = (playerMarker, index, square) => {
+        GameBoard.gameArray[index][0] = "taken";
+        GameBoard.gameArray[index][1] = player.marker;
+        square.innerText = player.marker;
+    };
+
+    const getRandomIntInclusive = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    };
+
+    const computerMove = (computerChoice) => {
+        let num = getRandomIntInclusive(0, 8); 
+        // if (GameBoard.gameArray[num][0] == "free") {
+        //     GameBoard.gameArray[num][0] = "taken";
+        //     GameBoard.gameArray[num][1] = computer.marker;
+        //     boardHtml[num].innerText = computer.marker;
+        // };
+
+        while (GameBoard.gameArray[num][0] != "free" && gameCount != 4) {
+            num = getRandomIntInclusive(0, 8);
+        }
+        if (gameCount < 4) {
+            GameBoard.gameArray[num][0] = "taken";
+            GameBoard.gameArray[num][1] = computer.marker;
+            boardHtml[num].innerText = computer.marker;
+        };
+        
+
+        console.log(num);
+    }
+    
+    const boardHtml = document.querySelectorAll(".game-table td");
 
     // Select choice buttons
     let choicesHtml = document.querySelectorAll(".choice");
+    let startHtml = document.querySelector("#start");
+    // Start game
     choicesHtml.forEach(choice => {
         choice.addEventListener("click", () => {
             player = playerFactory("player", choice.id);
@@ -39,19 +76,21 @@ const displayController = (() => {
             }
             computer = playerFactory("computer", computerChoice);
             console.log({player, computer});
-
-            let boardHtml = document.querySelectorAll(".game-table td");
             boardHtml.forEach((square, index) => {
                 square.addEventListener("click", () => {
-                gameBoard.gameArray[index][0] = "taken";
-                gameBoard.gameArray[index][1] = player.marker;
-                square.innerText = player.marker;
-                })
+                placeMarker(player.marker, index, square);
+                computerMove(computerChoice);
+                
+                gameCount += 1;
+                console.log({gameCount});
+            })
             })
         })
     });  
     
-    console.log({player, computer});
+    return {
+        player
+    };
     
 })();
 
